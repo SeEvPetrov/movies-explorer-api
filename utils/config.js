a56f7a12df3e12f54a97ad404127169109bcbdf8
Sergei Petrov <seevpetrov@gmail.com>
2022-10-09 22:24:47 +0300

    fix: доработка config

--- utils/config.js
+++ utils/config.js
@@ -7,7 +7,7 @@ const limiter = rateLimit({
   max: 1000,
 });
 
-const { JWT_SECRET = 'a0d09c51-712e-4f6f-b569-ab027c2c1fac', PORT = 3001, DATABASE_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { JWT_SECRET = 'a0d09c51-712e-4f6f-b569-ab027c2c1fac', PORT = 3000, DATABASE_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const SALT_LENGTH = 10;
 
 module.exports = {
